23dd8dfe97986ed6319e5addd8de4e97915be3a2
MercedesMoraleda <mercemm01@gmail.com>
2026-05-30 17:39:04 +0200

    add implement mis libros

--- Servidor/src/main/java/com/example/demo/Mapper/UserMapper.java
+++ Servidor/src/main/java/com/example/demo/Mapper/UserMapper.java
@@ -5,13 +5,15 @@ import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Component;
 
+import com.example.demo.Entity.LibroEntity;
 import com.example.demo.Entity.UserEntity;
 import com.example.demo.Model.InterfaceUserDTO;
+import com.example.demo.Model.LibroDTO;
 
 @Component
 public class UserMapper {
 	
-	public InterfaceUserDTO toDTO(UserEntity user) {
+	public InterfaceUserDTO userToDTO(UserEntity user) {
 
         List<String> generos = user.getUserGeneros()
                 .stream()
@@ -26,5 +28,22 @@ public class UserMapper {
                 user.getDescripcion(),
                 generos
         );
+    }
+	
+	public LibroDTO libroToDTO(LibroEntity libro) {
+
+        LibroDTO dto = new LibroDTO();
+
+        dto.setIdLibro(libro.getIdLibro());
+        dto.setTitulo(libro.getTitulo());
+        dto.setAutor(libro.getAutor());
+        dto.setEditorial(libro.getEditorial());
+        dto.setNumPaginas(libro.getNumPaginas());
+        dto.setSinopsis(libro.getSinopsis());
+        dto.setImagen(libro.getImagen());
+        dto.setFechaPublicacion(libro.getFechaPublicacion());
+        dto.setValoracion(libro.getValoracion());
+
+        return dto;
     }
 }
