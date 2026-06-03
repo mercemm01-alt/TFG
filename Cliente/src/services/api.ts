d42af507e2e01381cb35056eb5ab6e4beccfb113
MercedesMoraleda <mercemm01@gmail.com>
2026-06-03 20:32:04 +0200

    update api

--- Cliente/src/services/api.ts
+++ Cliente/src/services/api.ts
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:8080/api";
+const API_URL = "https://tfg-z11h.onrender.com/api";
 
 export const apiFetch = async (endpoint: string, options: RequestInit = {}) => {
     const response = await fetch(`${API_URL}${endpoint}`, {
