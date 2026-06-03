b499e7fe76aa5bacd70729c0f44fbe045ed77c3a
MercedesMoraleda <mercemm01@gmail.com>
2026-06-03 21:08:29 +0200

    update

--- Cliente/src/pages/Login/login.tsx
+++ Cliente/src/pages/Login/login.tsx
@@ -5,7 +5,7 @@ import { useState } from 'react';
 import './login.css'
 
 export const loginUser = (datos: LoginDTO) => {
-    return apiFetch("/login", {
+    return apiFetch("/", {
         method: "POST",
         body: JSON.stringify({
             user: datos.user.trim(),
